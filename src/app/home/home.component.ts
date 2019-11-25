9ca4b52e40291fe7e5309802879c0cb58d28f727
henriqueFutema <henrique.futema01@gmail.com>
2019-11-25 11:05:25 -0300

    Generate Controller

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,7 @@ export class HomeComponent implements OnInit {
   inpType: String = "";
   hasError: Boolean = false;
   fileUrl: any;
+  methodsController: Array<String> = ["store", "index", "update", "destroy"];
 
   ngOnInit() {}
 
@@ -51,12 +52,29 @@ export class HomeComponent implements OnInit {
   }
 
   handleSubmitController() {
-    let data = `const mongoose = require('mongoose'); const ${this.inpFileName}Schema = new mongoose.Schema({`;
-    this.arraysType.map(val => {
-      data = `${data} ${val.name}:{ type: ${val.type} },`;
-    });
+    const { inpFileName, methodsController } = this;
 
-    data += `}); module.exports = mongoose.model(${this.inpFileName});`;
+    let data = `const ${inpFileName} = require('../models/${inpFileName}'); module.exports = {`;
+
+    //POST
+    data += `async ${
+      methodsController[0]
+    }(req, res){ const ${inpFileName.toLowerCase()} = await ${inpFileName}.create(req.body); return res.json(${inpFileName.toLowerCase()}) },`;
+
+    //GET
+    data += `async ${
+      methodsController[1]
+    }(req, res){ const ${inpFileName.toLowerCase()} = await ${inpFileName}.find(); return res.json(${inpFileName.toLowerCase()})},`;
+
+    //PUT
+    data += `async ${
+      methodsController[2]
+    }(req, res){ const ${inpFileName.toLowerCase()} = await ${inpFileName}.findByIdAndUpdate(req.params.id, req.body, { new: true }); return res.json(${inpFileName.toLowerCase()}) },`;
+
+    //DELETE
+    data += `async ${
+      methodsController[3]
+    }(req, res){ const ${inpFileName.toLowerCase()} = await ${inpFileName}.findByIdAndDelete(req.params.id, req.body, { new: true }); return res.json({ ok: true }) }}`;
 
     const blob = new Blob([data], { type: "application/octet-stream" });
 
